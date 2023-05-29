2b5b04bb2fbb938c33753a57843a28a3d21510cb
dlara14 <davidmlara14@gmail.com>
2023-05-29 17:23:44 -0500

    Prueba

--- src/com/curso/Ejercicio1.java
+++ src/com/curso/Ejercicio1.java
@@ -1,2 +1,27 @@
-package com.curso;public class Ejercicio1 {
+package com.curso;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        String nombre = "David";
+        System.out.println(nombre);
+
+        int edad = 22;
+        System.out.println(edad);
+
+        long añoNacimiento = 2000;
+        System.out.println(añoNacimiento);
+
+        short mesNacimiento = 9;
+        System.out.println(mesNacimiento);
+
+        boolean soltero = true;
+        System.out.println(soltero);
+
+        double estatura = 1.62d;
+        System.out.println(estatura);
+
+
+    }
 }
